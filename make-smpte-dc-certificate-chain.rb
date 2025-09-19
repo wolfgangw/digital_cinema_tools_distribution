4d08c8eb9c334cb8c37e77cbb982f242755b7380
Wolfgang Woehl <wolfgang@delphine>
2025-09-19 10:47:46 +0200

    make-smpte-dc-certificate-chain: minor updates

--- make-smpte-dc-certificate-chain.rb
+++ make-smpte-dc-certificate-chain.rb
@@ -1,12 +1,14 @@
 #!/usr/bin/env ruby
 #
-# make-dc-certificate-chain.rb
+# make-smpte-dc-certificate-chain.rb
 #
-# Wolfgang Woehl 2010-2023
+# Creates digital cinema compliant certificates as specified
+# by SMPTE 430-2-2006 D-Cinema Operations -- Digital Certificate.
+# Leaf certificates will be useable for
+#   - XML signature as signer certificate
+#   - SM target for DKDM generation
 #
-# v0.2010.12.07
-# v1.2012.04.06.distribution
-# v1.2023.11.26
+# Wolfgang Woehl 2010-2025
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
@@ -21,75 +23,40 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #
+serials = Array.new
 #
-# Creates 3 related digital cinema compliant certificates as specified
-# by SMPTE 430-2-2006 D-Cinema Operations -- Digital Certificate.
-# Verifies root certificate -> intermediate -> leaf certificate relations
-# and concatenates into a certificate chain.
-#
-# Requires ruby and openssl
-#
-# Proof of concept. In real-world applications private keys 
-# would be protected with appropriate file permissions and
-# passphrases to restrict access.
-# Also they'd be part of a key- and certificate-handling
-# infrastructure to allow for various intermediate and leaf 
-# certificates and to enable the related tools to locate 
-# keys and certificates.
-#
-# The script, in principle, does the same thing 3 times,
-# once to create a root authority, then an intermediate
-# authority and finally a leaf at the end of the branch:
-#
-#   a) Create a private/public key pair
-#   b) define digital-cinema-specific properties of the corresponding, to-be-created certificate
-#   c) Issue a request for signature (The request holds the public key component plus metadata)
-#   d) Sign and thereby issue a signed certificate (self-signed in the case of the root certificate)
-#
-# The relation between 2 certificates is established in d)
-#
-# Intentionally no ruby-specific openssl bindings here,
-# merely building shell calls to openssl.
-#    - So why is this a ruby script then?
-#    - Because I nearly broke my bones trying to properly quote 
-#      the required dnq's in bash. Solutions much appreciated
-#
-# No directory and file checks here. Watch where you run it:
-#
-# $ mkdir certstore
-# $ cd certstore
-# $ make-dc-certificate-chain.rb # creates keys and certificates in the current directory
-#
-# Use leaf.key to sign files. For example:
-# $ xmlsec --sign --privkey-pem leaf.key --trusted-pem intermediate.signed.pem --trusted-pem ca.self-signed.pem presigned_cpl.xml
-# where presigned_cpl.xml is an XML Signature template, containing a 
-# composition playlist, a signer fragment and a signature block,
-# containing a signed info fragment and a signature value stub.
-#
-# See https://github.com/wolfgangw/digital_cinema_tools/blob/master/cinemaslides
-# for proof-of-concept code to encrypt essence track files and generate KDMs,
-# using these keys and certificates.
-#
+specified_serials_upper_bound = 2 ** 64
+xdc_serials_upper_bound = 2 ** 63 - 1 # Max value signed 64 bit integer
+dolby_dss220_serials_upper_bound = 2 ** 32 - 1
+my_serials_upper_bound = ( dolby_dss220_serials_upper_bound - 1 ) / 2 # Give them 1 more bit
+4.times do |n| serials << rand( my_serials_upper_bound ) end
+serials = serials.sort
 
+# do not reach into 10.2038 which is beyond unix timestamp issues occurring on 19.01.2038
+days = 4700
 
-# Clean up previous runs
-old = Dir.glob( [ 'ca.*', 'intermediate.*', 'leaf.*', 'dc-certificate-chain', 'signer.key' ] )
-old.each do |file|
-  File.delete( file )
+if ARGV.size != 1
+  puts "Domain name required (e.g. 'example.org')"
+  exit 1
+else
+  if ARGV.first =~ /.+\..+/
+    domain = ARGV.first
+  else
+    puts "Please specify domain name with 2 name components, separated with a period (e.g. 'example.org')"
+    exit 1
+  end
 end
 
+### Root CA
+#
+puts "Generating ca0 key..."
+`openssl genrsa -out #{ domain }.ca0.key 2048`
 
-### Make a self-signed root certificate. This will act as trust anchor for your certificate chain
-
-# Generate CA key pair (private and public component)
-`openssl genrsa -out ca.key 2048`
-
-# Note basicConstraints and keyUsage
-# pathlen restricts the length of a certificate chain that verifies back to this point
-ca_cnf = <<EOF
+ca0_cnf = <<EOF
 [ req ]
 distinguished_name = req_distinguished_name
 x509_extensions	= v3_ca
+string_mask = "default"
 [ v3_ca ]
 basicConstraints = critical,CA:true,pathlen:3
 keyUsage = keyCertSign,cRLSign
@@ -100,29 +67,36 @@ O = Unique organization name
 OU = Organization unit
 CN = Entity and dnQualifier
 EOF
-File.open( 'ca.cnf', 'w' ) { |f| f.write( ca_cnf ) }
+File.open( 'ca0.cnf', 'w' ) { |f| f.write( ca0_cnf ) }
 
-# Subject dnQualifier (Public key thumbprint, see SMPTE 430-2-2006 sections 5.3.1, 5.4 and DCI CTP section 2.1.11)
-ca_dnq = `openssl rsa -outform PEM -pubout -in ca.key | openssl base64 -d | dd bs=1 skip=24 2>/dev/null | openssl sha1 -binary | openssl base64`.chomp
-ca_dnq = ca_dnq.gsub( '/', '\/' ) # can have values like '0Za8/aABE05Aroz7le1FOpEdFhk=', note the '/'. protect for name parser
-# Note the absence of role indicators in CA's CN (See SMPTE 430-2-2006 Annex A, CommonName Role Descriptions)
-ca_subject = '/O=example.org/OU=example.org/CN=.smpte-430-2.ROOT.NOT_FOR_PRODUCTION/dnQualifier=' + ca_dnq
+ca0_dnq = `openssl rsa -outform PEM -pubout -in #{ domain }.ca0.key | openssl base64 -d | dd bs=1 skip=24 2>/dev/null | openssl sha1 -binary | openssl base64`.chomp
+ca0_dnq = ca0_dnq.gsub( '/', '\/' )
+puts ca0_dnq.inspect
+#
+# this is broken - the quoting protection does not make it through
+# thus, whenever there is a '+' in a generated thumbprint, the
+# cert will fail for dnQualifier mismatch
+# workaround for now: generate new keys/certs until chance gives
+# you a set of thumbprints without '+'
+#
+ca0_dnq = ca0_dnq.gsub( '+', '\+' )
+puts ca0_dnq.inspect
+ca0_subject = "/O=#{ domain }/OU=#{ domain }/CN=.ca0.#{ domain }/dnQualifier=" + ca0_dnq
 
-# Generate self-signed certificate
-`openssl req -new -x509 -sha256 -config ca.cnf -days 3650 -set_serial 5 -subj "#{ ca_subject }" -key ca.key -outform PEM -out ca.self-signed.pem`
+puts "Generating ca0 PEM..."
+`openssl req -new -x509 -sha256 -config ca0.cnf -days #{ days } -set_serial #{ serials[0] } -subj "#{ ca0_subject }" -key #{ domain }.ca0.key -outform PEM -out #{ domain }.ca0.pem`
 ###
 
 
-### Make an intermediate certificate, issued/signed by the root certificate and root's private key
+### Intermediate CA
+puts "Generating ca1 key..."
+`openssl genrsa -out #{ domain }.ca1.key 2048`
 
-# Generate intermediate's key pair
-`openssl genrsa -out intermediate.key 2048`
-
-# Note basicConstraints pathlen
-inter_cnf = <<EOF
+ca1_cnf = <<EOF
 [ default ]
 distinguished_name	= req_distinguished_name
 x509_extensions	= v3_ca
+string_mask = "default"
 [ v3_ca ]
 basicConstraints = critical,CA:true,pathlen:2
 keyUsage = keyCertSign,cRLSign
@@ -133,36 +107,40 @@ O = Unique organization name
 OU = Organization unit
 CN = Entity and dnQualifier
 EOF
-File.open( 'intermediate.cnf', 'w' ) { |f| f.write( inter_cnf ) }
+File.open( 'ca1.cnf', 'w' ) { |f| f.write( ca1_cnf ) }
 
-# equiv.   openssl x509 -pubkey -noout -in intermediate.signed.pem | openssl base64 -d | dd bs=1 skip=24 2>/dev/null | openssl sha1 -binary | openssl base64
-inter_dnq = `openssl rsa -outform PEM -pubout -in intermediate.key | openssl base64 -d | dd bs=1 skip=24 2>/dev/null | openssl sha1 -binary | openssl base64`.chomp
-inter_dnq = inter_dnq.gsub( '/', '\/' )
-# Note the absence of role indicators in CA's CN (See SMPTE 430-2-2006 Annex A, CommonName Role Descriptions)
-inter_subject = "/O=example.org/OU=example.org/CN=.smpte-430-2.INTERMEDIATE.NOT_FOR_PRODUCTION/dnQualifier=" + inter_dnq
+ca1_dnq = `openssl rsa -outform PEM -pubout -in #{ domain }.ca1.key | openssl base64 -d | dd bs=1 skip=24 2>/dev/null | openssl sha1 -binary | openssl base64`.chomp
+ca1_dnq = ca1_dnq.gsub( '/', '\/' )
+puts ca1_dnq.inspect
+#
+# this is broken - the quoting protection does not make it through
+# thus, whenever there is a '+' in a generated thumbprint, the
+# cert will fail for dnQualifier mismatch
+# workaround for now: generate new keys/certs until chance gives
+# you a set of thumbprints without '+'
+#
+ca1_dnq = ca1_dnq.gsub( '+', '\+' )
+puts ca1_dnq.inspect
+ca1_subject = "/O=#{ domain }/OU=#{ domain }/CN=.ca1.#{ domain }/dnQualifier=" + ca1_dnq
 
-# Request signing for intermediate certificate
-`openssl req -new -config intermediate.cnf -days 3649 -subj "#{ inter_subject }" -key intermediate.key -out intermediate.csr`
-# Issue/Sign with root certificate/private key
-`openssl x509 -req -sha256 -days 3649 -CA ca.self-signed.pem -CAkey ca.key -set_serial 6 -in intermediate.csr -extfile intermediate.cnf -extensions v3_ca -out intermediate.signed.pem`
+puts "Generating ca1 CSR..."
+`openssl req -new -config ca1.cnf -subj "#{ ca1_subject }" -key #{ domain }.ca1.key -out #{ domain }.ca1.csr`
+puts "Generating ca1 PEM..."
+`openssl x509 -req -sha256 -days #{ days - 1 } -CA #{ domain }.ca0.pem -CAkey #{ domain }.ca0.key -set_serial #{ serials[1] } -in #{ domain }.ca1.csr -extfile ca1.cnf -extensions v3_ca -out #{ domain }.ca1.pem`
 ###
 
 
-### Make a leaf certificate, issued/signed by the intermediate certificate and intermediate private key
-# Use leaf's private key to sign content (like CPLs, PKLs and KDMs)
+### Signer (CS) leaf certificate
+puts "Generating cs key..."
+`openssl genrsa -out #{ domain }.cs.key 2048`
 
-# Generate leaf's key pair
-# Some apps require passphrase-protected signing keys. Add the -des3 switch to interactively provide a passphrase.
-`openssl genrsa -out leaf.key 2048`
-
-# Note basicConstraints and keyUsage
-leaf_cnf = <<EOF
+cs_cnf = <<EOF
 [ default ]
 distinguished_name	= req_distinguished_name
 x509_extensions	= v3_ca
+string_mask = "default"
 [ v3_ca ]
-# pathlen not needed. Would the cert be rejected because of pathlen marked critical?
-# Answer: no. see SMPTE 430-2-2006 section 6.2 Validation Rules - Check 5
+# See SMPTE 430-2-2006 section 6.2 Validation Rules - Check 5
 basicConstraints = critical,CA:false
 keyUsage = digitalSignature,keyEncipherment
 subjectKeyIdentifier = hash
@@ -172,48 +150,95 @@ O = Unique organization name
 OU = Organization unit
 CN = Entity and dnQualifier
 EOF
-File.open( 'leaf.cnf', 'w' ) { |f| f.write( leaf_cnf ) }
+File.open( 'cs.cnf', 'w' ) { |f| f.write( cs_cnf ) }
 
-# equiv.  openssl x509 -pubkey -noout -in leaf.signed.pem | openssl base64 -d | dd bs=1 skip=24 2>/dev/null | openssl sha1 -binary | openssl base64
-leaf_dnq = `openssl rsa -outform PEM -pubout -in leaf.key | openssl base64 -d | dd bs=1 skip=24 2>/dev/null | openssl sha1 -binary | openssl base64`.chomp
-leaf_dnq = leaf_dnq.gsub( '/', '\/' )
-# Note the role indicator in CN (CS=content signer)
-# See Digital Cinema System Specification v1.1 section 9.4.3.5. Functions of the Security Manager (SM) part 4:
+cs_dnq = `openssl rsa -outform PEM -pubout -in #{ domain }.cs.key | openssl base64 -d | dd bs=1 skip=24 2>/dev/null | openssl sha1 -binary | openssl base64`.chomp
+cs_dnq = cs_dnq.gsub( '/', '\/' )
+puts cs_dnq.inspect
 #
-#   Validate Composition Playlists (CPL), and log results as a prerequisite to preparing the suite 
-#   for the associated composition playback. For encrypted content, validation shall be by cross 
-#   checking that the associated KDM's ContentAuthenticator element matches a certificate thumbprint 
-#   of one of the certificates in the CPL's signer chain (see item 1 above), and that such certificate
-#   indicate only a "Content Signer" (CS) role per Section 5.3.4, "Naming and Roles" of the certificate 
-#   specification (SMPTE430-2 D-Cinema Operation - Digital Certificate).
+# this is broken - the quoting protection does not make it through
+# thus, whenever there is a '+' in a generated thumbprint, the
+# cert will fail for dnQualifier mismatch
+# workaround for now: generate new keys/certs until chance gives
+# you a set of thumbprints without '+'
 #
-# Roles are separated by space character and by leftmost period character from the unique entity label
-leaf_subject = "/O=example.org/OU=example.org/CN=CS.smpte-430-2.LEAF.NOT_FOR_PRODUCTION/dnQualifier=" + leaf_dnq
+cs_dnq = cs_dnq.gsub( '+', '\+' )
+puts cs_dnq.inspect
+cs_subject = "/O=#{ domain }/OU=#{ domain }/CN=CS.#{ domain }/dnQualifier=" + cs_dnq
 
-# Request signing for leaf certificate
-`openssl req -new -config leaf.cnf -days 3648 -subj "#{ leaf_subject }" -key leaf.key -outform PEM -out leaf.csr`
-# Issue/Sign with intermediate certificate/private key
-`openssl x509 -req -sha256 -days 3648 -CA intermediate.signed.pem -CAkey intermediate.key -set_serial 7 -in leaf.csr -extfile leaf.cnf -extensions v3_ca -out leaf.signed.pem`
+puts "Generating cs CSR..."
+`openssl req -new -config cs.cnf -subj "#{ cs_subject }" -key #{ domain }.cs.key -outform PEM -out #{ domain }.cs.csr`
+puts "Generating cs PEM..."
+`openssl x509 -req -sha256 -days #{ days - 2 } -CA #{ domain }.ca1.pem -CAkey #{ domain }.ca1.key -set_serial #{ serials[2] } -in #{ domain }.cs.csr -extfile cs.cnf -extensions v3_ca -out #{ domain }.cs.pem`
 ###
 
 
+### Target (SM) leaf certificate
+puts "Generating sm key..."
+`openssl genrsa -out #{ domain }.sm.key 2048`
+
+sm_cnf = <<EOF
+[ default ]
+distinguished_name	= req_distinguished_name
+x509_extensions	= v3_ca
+string_mask = "default"
+[ v3_ca ]
+# See SMPTE 430-2-2006 section 6.2 Validation Rules - Check 5
+basicConstraints = critical,CA:false
+keyUsage = digitalSignature,keyEncipherment
+subjectKeyIdentifier = hash
+authorityKeyIdentifier = keyid,issuer:always
+[ req_distinguished_name ]
+O = Unique organization name
+OU = Organization unit
+CN = Entity and dnQualifier
+EOF
+File.open( 'sm.cnf', 'w' ) { |f| f.write( sm_cnf ) }
+
+sm_dnq = `openssl rsa -outform PEM -pubout -in #{ domain }.sm.key | openssl base64 -d | dd bs=1 skip=24 2>/dev/null | openssl sha1 -binary | openssl base64`.chomp
+sm_dnq = sm_dnq.gsub( '/', '\/' )
+puts sm_dnq.inspect
+#
+# this is broken - the quoting protection does not make it through
+# thus, whenever there is a '+' in a generated thumbprint, the
+# cert will fail for dnQualifier mismatch
+# workaround for now: generate new keys/certs until chance gives
+# you a set of thumbprints without '+'
+#
+sm_dnq = sm_dnq.gsub( '+', '\+' )
+puts sm_dnq.inspect
+sm_subject = "/O=#{ domain }/OU=#{ domain }/CN=SM.#{ domain }/dnQualifier=" + sm_dnq
+
+puts "Generating sm CSR..."
+`openssl req -new -config sm.cnf -subj "#{ sm_subject }" -key #{ domain }.sm.key -outform PEM -out #{ domain }.sm.csr`
+puts "Generating sm PEM..."
+`openssl x509 -req -sha256 -days #{ days - 2 } -CA #{ domain }.ca1.pem -CAkey #{ domain }.ca1.key -set_serial #{ serials[3] } -in #{ domain }.sm.csr -extfile sm.cnf -extensions v3_ca -out #{ domain }.sm.pem`
+###
+
+
+### Keep configuration and CSRs
+`mkdir confs csrs ; mv ./*.cnf confs/ ; mv ./*.csr csrs/`
+
 ### Print out issuer and subject information for each of the generated certificates
 puts "\n+++ Certificate info +++\n"
-[ [ 'Self-signed CA certificate (issuer == subject)', 'ca.self-signed.pem' ], [ 'Intermediate certificate', 'intermediate.signed.pem' ], [ 'Leaf certificate', 'leaf.signed.pem' ] ].each do |t|
+[ [ 'CA 0', "#{ domain }.ca0.pem" ], [ 'CA 1', "#{ domain }.ca1.pem" ], [ 'Signer', "#{ domain }.cs.pem" ], [ 'Target/Lab', "#{ domain }.sm.pem" ] ].each do |t|
   puts "\n#{ t.first } (#{ t.last }):\n#{ `openssl x509 -noout -subject -in #{ t.last }` }   signed by\n #{ `openssl x509 -noout -issuer -in #{ t.last }` }"
 end
 
 
 # For illustration: Verify certificates and write certificate chain
 puts "\n+++ Verify certificates and write dc-certificate-chain +++\n\n"
-puts `openssl verify -CAfile ca.self-signed.pem ca.self-signed.pem`
-`cp ca.self-signed.pem dc-certificate-chain`
-puts `openssl verify -CAfile dc-certificate-chain intermediate.signed.pem`
-`cat intermediate.signed.pem >> dc-certificate-chain`
-puts `openssl verify -CAfile dc-certificate-chain leaf.signed.pem`
-`cat leaf.signed.pem >> dc-certificate-chain`
+puts `openssl verify -CAfile #{ domain }.ca0.pem #{ domain }.ca0.pem`
+`cp #{ domain }.ca0.pem #{ domain }.cs.chain.cert`
+puts `openssl verify -CAfile #{ domain }.cs.chain.cert #{ domain }.ca1.pem`
+`cat #{ domain }.ca1.pem >> #{ domain }.cs.chain.cert`
+puts `openssl verify -CAfile #{ domain }.cs.chain.cert #{ domain }.cs.pem`
+`cat #{ domain }.cs.pem >> #{ domain }.cs.chain.cert`
 
-`ln -s leaf.key signer.key`
+`cp #{ domain }.ca0.pem #{ domain }.sm.chain.cert`
+`cat #{ domain }.ca1.pem >> #{ domain }.sm.chain.cert`
+`cat #{ domain }.sm.pem >> #{ domain }.sm.chain.cert`
+puts `openssl verify -CAfile #{ domain }.sm.chain.cert #{ domain }.sm.pem`
 
 puts "\nDONE"
 
